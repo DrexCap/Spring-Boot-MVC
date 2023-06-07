d191ef7d65e89d433a7a9823a19c10cc7f727715
Andre Castillo <andre.ac679@gmail.com>
2023-06-06 21:35:23 -0500

    modificando pom.xml

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, "Andre", "Castillo", "andre@gmail.com", '2023-08-16');
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, "Carlos", "Montes", "carlos@gmail.com", '2023-10-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Andre', 'Castillo', 'andre@gmail.com', '2023-08-16');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'Carlos', 'Montes', 'carlos@gmail.com', '2023-10-28');
